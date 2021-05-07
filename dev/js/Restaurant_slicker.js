1a36ec04a8a998ebc6d238c29d48b70f62eda3ff
j86013999 <pinpin1002@hotmail.com>
2021-05-07 22:08:18 +0800

    改檔案標題

---
+++ dev/js/Restaurant_slicker.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $('.envPictures').slick({
+    dots: true,
+    infinite: true,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 4000,
+    prevArrow: false,
+    nextArrow: false,
+    pauseOnFocus: false, 
+    pauseOnHover: false
+  });
+});
