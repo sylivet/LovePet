8bc1cfefc804a718d68b1400cff6470d4f31c8c0
sylivet <yuwenlavie@gmail.com>
2021-05-10 19:27:56 +0800

    還原

--- dev/js/clinic_faq.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-  $(".c_flip").click(function(){
-      $(this).next().slideToggle("slow");
-  });
-});
-// 改成點擊三角形才打開  
--- dev/js/hotel.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-  $(".h_flip").click(function(){
-      $(this).next().slideToggle("slow");
-  });  
-});
-// 改成點擊三角形才打開        
