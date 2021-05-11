524187a465f5cf2694026a1512810101adaeab8c
ozizi0106o <ozizi0106o@gmail.com>
2021-05-11 17:59:49 +0800

    修改旅館+健檢 FAQ

--- dev/js/clinic_faq.js
+++ dev/js/clinic_faq.js
@@ -1,5 +1,14 @@
+/*----- 健檢中心Q&A-----*/
 $(function(){
-  $(".c_flip").click(function(){      
-      $(this).next().slideToggle("slow");
-  });
+  $('.c_show').click(function(){
+      $(this).next().slideToggle('slow'); 
+      $(this).find('.c_click1').toggleClass('c_rotate1');
+  });  
+});
+
+$(function(){
+  $('.c_flip').click(function(){
+      $(this).next().slideToggle('slow');
+      $(this).find('.c_click').toggleClass('c_rotate');
+  });  
 });
--- dev/js/hotel.js
+++ dev/js/hotel.js
@@ -1,5 +1,14 @@
+/*----- 旅館Q&A-----*/
 $(function(){
-  $(".h_flip").click(function(){
-      $(this).next().slideToggle("slow");
+  $('.h_show').click(function(){
+      $(this).next().slideToggle('slow'); 
+      $(this).find('.h_click1').toggleClass('h_rotate1');
   });  
-});   
+});
+
+$(function(){
+  $('.h_flip').click(function(){
+      $(this).next().slideToggle('slow');
+      $(this).find('.h_click').toggleClass('h_rotate');
+  });  
+});
