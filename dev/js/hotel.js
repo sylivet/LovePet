2a9d03c5efb5f90042a753f0f1053e5ff14230d7
ozizi0106o <ozizi0106o@gmail.com>
2021-05-13 20:00:29 +0800

    調整RWD+下拉式

--- dev/js/hotel.js
+++ dev/js/hotel.js
@@ -6,21 +6,34 @@
 /* ----- 房型選擇跟介紹-----*/
 $(function(){
   $('.h_dropdown').click(function(){
-      $(this).parent().toggleClass('-open');
+      $(this).parent().toggleClass('h_openmenu');
   });  
 });
+
+$(function(){
+  $('.h_content').click(function(){
+      $(this).parent().parent().toggleClass('h_openmenu').find('.h_dropdown').text($(this).text());
+  });  
+});
+// 換字完 格式跑掉
+// 選項內的框框要彈回去
+// 怎麼切頁
+
+
+
+
 
 /*----- 旅館Q&A-----*/
 $(function(){
   $('.h_show').click(function(){
       $(this).next().slideToggle('slow'); 
-      $(this).find('.h_click1').toggleClass('h_rotate1');
+      $(this).parent().toggleClass('h_faqopen');
   });  
 });
 
 $(function(){
   $('.h_flip').click(function(){
       $(this).next().slideToggle('slow');
-      $(this).find('.h_click').toggleClass('h_rotate');
+      $(this).parent().toggleClass('h_faqopen0');
   });  
 });
