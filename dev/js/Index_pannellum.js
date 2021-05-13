4db34a1f12a3d114beca92f60128b0de94d622b6
sylivet <yuwenlavie@gmail.com>
2021-05-13 23:27:56 +0800

    [fix]修正鼠標、[add]首頁環景熱點

--- dev/js/Index_pannellum.js
+++ dev/js/Index_pannellum.js
@@ -1,11 +1,28 @@
 pannellum.viewer = pannellum.viewer("panorama", {
     type: "equirectangular",
     panorama: "img/index/panorama.jpg",
+    "pitch": -10,
+    "hfov": 180,
     compass: true,
     autoLoad: true,
     showControls: false,
     mouseZoom: false,
-    draggable: false
+    draggable: false,
+    // "hotSpotDebug": true,
+    "hotSpots": [
+        {
+            "pitch": -8.5,
+            "yaw": 8.7,
+            "type": "info",
+            "text": "貓抓柱",
+        },
+        {
+            "pitch": -20,
+            "yaw": 46,
+            "type": "info",
+            "text": "寵物窩墊"
+        }
+    ]
 });
 // Make buttons work
 document.getElementById("pan-up").addEventListener("click", function (e) {
