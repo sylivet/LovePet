de759ac2f6dbc17ff3904a7253558de6fc98ca33
sylivet <yuwenlavie@gmail.com>
2021-05-14 23:19:53 +0800

    no message

--- dev/js/cursor.js
+++ dev/js/cursor.js
@@ -7,5 +7,9 @@ $(document).ready(function() {
 					top: e.pageY - cursor.height() / 2,
 					left: e.pageX - cursor.width() / 2
 				})
+				.removeClass("cursor-center");
 		})
+		.mouseleave(function() {
+			cursor.addClass("cursor-center");
+		});
 });
