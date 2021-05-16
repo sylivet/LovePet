aa8f61e0d86760761169c92ebf6326588cfe04f2
sylivet <yuwenlavie@gmail.com>
2021-05-16 12:43:24 +0800

    [add]後台

---
+++ dev/js/index_vue.js
@@ -0,0 +1,33 @@
+// 最新消息
+let vm = new Vue({
+    el: "#index", 
+    data: {
+        news:[
+            {
+                imgURL:"img/index/news1.jpg",
+                title:"特製毛孩麵包上市",
+                content:"無鹽鬆軟麵包，不使用小麥粉，使用有機南瓜、北海道馬鈴薯、京都山羊乳與蜂蜜等天然食材製成。",
+            },
+            {
+                imgURL:"img/index/news2.jpg",
+                title:"即日起游泳池開放",
+                content:"炎炎夏日讓毛孩玩玩水，清涼又有趣。免費提供寵物救生衣以及浮板玩具。",
+            },
+            {
+                imgURL:"img/index/news3.jpg",
+                title:"與主人共享舒適房間",
+                content:"我們不惜成本打造合適主人與寵物的居住環境，期望毛孩們獲得最滿意的外宿經驗。",
+            },
+            {
+                imgURL:"img/index/news4.jpg",
+                title:"專業寵物健檢中心",
+                content:"寵物的健康不能等！樂寵擁有最專業的獸醫駐足在渡假村內，帶毛孩來檢查一番，還能自由地奔跑在園區內，增進抵抗力。",
+            },
+            {
+                imgURL:"img/index/news5.jpg",
+                title:"毛孩沙拉新上市",
+                content:"夏季限定「雞肉沙拉」新上市！炎炎夏日，你還在餵家裡的毛孩寵物飼料嗎？趕快預訂樂寵的鮮食，讓牠清涼一下。",
+            },
+        ],
+    }
+});
