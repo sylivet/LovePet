7c28eee9cb7d1a4854da1081bb099cb65dedb8e4
eddieco-cpu <eddie01241@gmail.com>
2021-05-18 10:16:58 +0800

    updated

--- dev/js/shopping_cart.js
+++ dev/js/shopping_cart.js
@@ -1,13 +1,19 @@
 var shoppingcart=document.getElementById("i_shoppingCart");
+
 shoppingcart.addEventListener("click",function(){
+
 var shoppingcartbk=document.getElementById("i_shoppingCart_bk");
 if(shoppingcartbk.style.display === "none"){
     shoppingcartbk.style.display = "block";
 }
-var close=document.getElementsByClassName("i_closeButton")[0];
-close.addEventListener("click",function(){
-    if(shoppingcartbk.style.display === "block"){
-        shoppingcartbk.style.display = "none";
-    }
-});
+
+var close=document.getElementsByClassName("i_closeButton");
+for(let i=0;i<close.length;i++){
+    close[i].addEventListener("click",function(){
+        if(this.closest('.i_background').style.display === "block"){
+            this.closest('.i_background').style.display = "none";
+        }
+    });
+}
+
 });
