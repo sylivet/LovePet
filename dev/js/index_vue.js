e84184b97e6a28717a74492acd0645851e2869a1
sylivet <yuwenlavie@gmail.com>
2021-05-21 13:25:08 +0800

    更改圖片大小/路徑

--- dev/js/index_vue.js
+++ dev/js/index_vue.js
@@ -10,19 +10,19 @@ let vm = new Vue({
           "無鹽鬆軟麵包，不使用小麥粉，使用有機南瓜、北海道馬鈴薯、京都山羊乳與蜂蜜等天然食材製成。",
       },
       {
-        imgURL: "img/index/news2.jpg",
+        imgURL: "img/index/news2.png",
         title: "即日起游泳池開放",
         content:
           "炎炎夏日讓毛孩玩玩水，清涼又有趣。免費提供寵物救生衣以及浮板玩具。",
       },
       {
-        imgURL: "img/index/news3.jpg",
+        imgURL: "img/index/news3.png",
         title: "與主人共享舒適房間",
         content:
           "我們不惜成本打造合適主人與寵物的居住環境，期望毛孩們獲得最滿意的外宿經驗。",
       },
       {
-        imgURL: "img/index/news4.jpg",
+        imgURL: "img/index/news4.png",
         title: "專業寵物健檢中心",
         content:
           "寵物的健康不能等！樂寵擁有最專業的獸醫駐足在渡假村內，帶毛孩來檢查一番，還能自由地奔跑在園區內，增進抵抗力。",
