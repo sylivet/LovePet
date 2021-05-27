98a4ebcd3518bbd5cfabd8248ba48e0cd066eeb6
sylivet <yuwenlavie@gmail.com>
2021-05-27 21:41:20 +0800

    ˊ_>ˋ

--- dev/js/hotel_vue.js
+++ dev/js/hotel_vue.js
@@ -1,13 +1,94 @@
+//房間預約元件
+Vue.component("room-booking", {
+    props: ['foods'],
+    template: "#roombookingbox",
+    methods: {
+        closeBox() {
+            this.$emit('closelightbox')
+        }
+    },
+    computed: {
+        totalPrice() {
+            var result = this.foods.reduce(function (a, b) {
+                return a + b.MEAL_PRICE * b.MEAL_COUNT;
+            }, 0)
+            return result;
+        },
+        filterCataToPets() {
+            return this.foods.filter(food => food.MEAL_CATA === "petsFood")
+        },
+        filterCataToHuman() {
+            return this.foods.filter(food => food.MEAL_CATA === "humanFood")
+        },
+    }
+})
+
+
 /*----- 720度環景 -----*/
 let vm = new Vue({
     el: "#app",
-    mounted() {        
+    data: {
+        rooms: [
+            {
+                ROOM_TYPE_ID:1,
+                ROOM_NAME:"時毛玩意",
+                PRICE:5999,
+                MAX_CAPACITY:4,
+                ROOM_IMG:"img/hotel/h_room1.png",
+                ROOM_IMG2:"img/hotel/h_room2.png",
+                ROOM_IMG3:"img/hotel/h_room3.png",
+                PANNELLUM:"img/hotel/h_panorama1.jpg",
+            },
+            {
+                ROOM_TYPE_ID:2,
+                ROOM_NAME:"一汪無際",
+                PRICE:3500,
+                MAX_CAPACITY:2,
+                ROOM_IMG:"img/hotel/h_room1.png",
+                ROOM_IMG2:"img/hotel/h_room2.png",
+                ROOM_IMG3:"img/hotel/h_room3.png",
+                PANNELLUM:"img/hotel/h_panorama1.jpg",
+            },
+            {
+                ROOM_TYPE_ID:3,
+                ROOM_NAME:"天羅地汪",
+                PRICE:4000,
+                MAX_CAPACITY:2,
+                ROOM_IMG:"img/hotel/h_room1.png",
+                ROOM_IMG2:"img/hotel/h_room2.png",
+                ROOM_IMG3:"img/hotel/h_room3.png",
+                PANNELLUM:"img/hotel/h_panorama1.jpg",
+            },
+            {
+                ROOM_TYPE_ID:4,
+                ROOM_NAME:"青喵淡寫",
+                PRICE:4999,
+                MAX_CAPACITY:4,
+                ROOM_IMG:"img/hotel/h_room1.png",
+                ROOM_IMG2:"img/hotel/h_room2.png",
+                ROOM_IMG3:"img/hotel/h_room3.png",
+                PANNELLUM:"img/hotel/h_panorama1.jpg",
+            },
+            {
+                ROOM_TYPE_ID:5,
+                ROOM_NAME:"舒毛一夏",
+                PRICE:4200,
+                MAX_CAPACITY:2,
+                ROOM_IMG:"img/hotel/h_room1.png",
+                ROOM_IMG2:"img/hotel/h_room2.png",
+                ROOM_IMG3:"img/hotel/h_room3.png",
+                PANNELLUM:"img/hotel/h_panorama1.jpg",
+            },
+        ],
+        allRoomMenu: [],
+    },
+    mounted() {
         pannellum.viewer = pannellum.viewer("h_panorama", {
             type: "equirectangular",
             panorama: "img/hotel/h_panorama1.jpg",
             // 調整初始畫面位置
             pitch: -10,
-            hfov: 180,  
+            hfov: 180,
             // 自動旋轉
             autoRotate: -2,
             // 指南針
@@ -15,7 +96,7 @@ let vm = new Vue({
             // 自動加載
             autoLoad: true,
             // 按鈕調整畫面
-            showControls: false, 
+            showControls: false,
             // 預防無限環景設定
             mouseZoom: false,
             draggable: false,
@@ -35,25 +116,25 @@ let vm = new Vue({
                 },
             ],
         });
-          
+
         // Make buttons work (按鈕調整移動畫面)
         document.getElementById("h_arrow_up").addEventListener("click", function (e) {
-        pannellum.viewer.setPitch(pannellum.viewer.getPitch() + 15);
+            pannellum.viewer.setPitch(pannellum.viewer.getPitch() + 15);
         });
         document.getElementById("h_arrow_down").addEventListener("click", function (e) {
-        pannellum.viewer.setPitch(pannellum.viewer.getPitch() - 15);
+            pannellum.viewer.setPitch(pannellum.viewer.getPitch() - 15);
         });
         document.getElementById("h_arrow_left").addEventListener("click", function (e) {
-        pannellum.viewer.setYaw(pannellum.viewer.getYaw() - 15);
+            pannellum.viewer.setYaw(pannellum.viewer.getYaw() - 15);
         });
         document.getElementById("h_arrow_right").addEventListener("click", function (e) {
-        pannellum.viewer.setYaw(pannellum.viewer.getYaw() + 15);
+            pannellum.viewer.setYaw(pannellum.viewer.getYaw() + 15);
         });
         document.getElementById("h_enlarge").addEventListener("click", function (e) {
-        pannellum.viewer.setHfov(pannellum.viewer.getHfov() - 15);
+            pannellum.viewer.setHfov(pannellum.viewer.getHfov() - 15);
         });
         document.getElementById("h_narrow").addEventListener("click", function (e) {
-        pannellum.viewer.setHfov(pannellum.viewer.getHfov() + 15);
+            pannellum.viewer.setHfov(pannellum.viewer.getHfov() + 15);
         });
     },
 });
