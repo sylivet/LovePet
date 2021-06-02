1efc3a08af3cd16fb74c6bc63a979fc91e614527
yuchen820304 <60634787+yuchen820304@users.noreply.github.com>
2021-06-02 20:56:26 +0800

    測試

---
+++ dev/js/header.js
@@ -0,0 +1,19 @@
+// Vue.component('the-cart', {
+//   template: '#cart',
+//   methods: {
+//     plus() {
+//       this.info[0].count++;
+//     },
+//     sub() {
+//       if (this.info[0].count > 0) {
+//         this.info[0].count--;
+//       } else {
+//         this.info[0].count == 0;
+//       }
+//     },
+//   },
+// });
+
+// new Vue({
+//   el: '#forhead',
+// });
