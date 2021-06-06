815ba0ba7ebb9b9caff4e39bbc045f5dff157ac0
tonyke1016 <ko3chutony831016@gmail.com>
2021-06-06 21:41:57 +0800

    no

--- dev/php/I_conn.php
+++ dev/php/I_conn.php
@@ -3,7 +3,7 @@
 //MySQL相關資訊
 $db_host = "127.0.0.1";
 $db_user = "root";
-$db_pass = "tibamepassword";
+$db_pass = "password";
 $db_select = "LOVE_PET";
 
 //建立資料庫連線物件
--- dev/php/R_conn.php
+++ dev/php/R_conn.php
@@ -4,7 +4,7 @@
   $db_host = "127.0.0.1";
   $db_user = "root";
   $db_select = "LOVE_PET";
-  $db_pass = "tibamepassword";
+  $db_pass = "password";
 
   //建立資料庫連線物件
   $dsn = "mysql:host=".$db_host.";dbname=".$db_select;
