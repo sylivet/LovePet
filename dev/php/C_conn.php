ab07a425b5a7988176a3949789a52767fe78194c
tonyke1016 <ko3chutony831016@gmail.com>
2021-06-06 11:46:01 +0800

    modify

--- dev/php/C_conn.php
+++ dev/php/C_conn.php
@@ -4,11 +4,7 @@
   $db_host = "127.0.0.1";
   $db_user = "root";
   $db_select = "LOVE_PET";
-<<<<<<< HEAD
   $db_pass = "password";
-=======
-  $db_pass = "ac1234zxc";
->>>>>>> 4d5ad7b2dc1486b19a3da6f7f5ed644a1aa592a5
 
   //建立資料庫連線物件
   $dsn = "mysql:host=".$db_host.";dbname=".$db_select;
