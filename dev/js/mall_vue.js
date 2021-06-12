904d9123a86d5dd7430d4e597da33bb61e95f83e
yuchen820304 <60634787+yuchen820304@users.noreply.github.com>
2021-06-13 01:24:18 +0800

    購物車全數完成/結帳頁面完成/購物車CSS修改/食品頁面修改成直接加入購物車

--- dev/js/mall_vue.js
+++ dev/js/mall_vue.js
@@ -3,7 +3,7 @@ const storage = localStorage;
 
 Vue.component('the-cart', {
   template: '#cart',
-  props: ['counts'], //counts接chooseItem陣列
+  props: ['counts', 'total'], //counts接chooseItem陣列
   methods: {
     plus(index) {
       this.$emit('to-plus', index); //子元件傳值給父元件用$emit(自訂事件)
@@ -18,31 +18,13 @@ Vue.component('the-cart', {
       this.$emit('to-remove', index);
       console.log('有偵測到刪除功能');
     },
-    // getCount(index) {
-    //   this.$emit('show-count', index);
-    // },
   },
-  computed: {
-    currentPrice() {
-      // for (let i = 0; i < this.counts.length; i++) {
-      //   return this.counts[i].count * this.counts[i].PRODUCT_PRICE;
-      // }
-    },
-    // getCount() {
-    //   let count = JSON.parse(storage.getItem('items'));
-    //   console.log(count);
-    //   return count.item_count;
-    // },
-    getPrice() {
-      // return JSON.parse(storage.getItem('item_0')).PRODUCT_PRICE;
-    },
-    getIMG() {
-      // return JSON.parse(storage.getItem('item_0')).PRODUCT_IMG;
-    },
-    getTotalPrice() {
-      return 100;
-    },
-  },
+});
+
+// ======數字轉千分符======
+Vue.filter('currency', function (price) {
+  return parseInt(price).toLocaleString('en-US');
+  //先轉成數字，再讓toLocaleString內建的轉成字串方法執行千分符並顯示，沒加parseInt在這會失敗
 });
 
 let data = {
@@ -69,6 +51,7 @@ let data = {
   chooseItem: [], //被選擇加入的商品
   page: 0,
   currentPage: [],
+  total: 0, //全部商品總額，初值為0
 };
 
 new Vue({
@@ -88,7 +71,12 @@ new Vue({
       self.food = res.data;
     });
   },
-  mounted() {},
+  mounted() {
+    this.chooseItem = JSON.parse(localStorage.getItem('items'));
+    //因為跳頁後整個new Vue會重跑，在這時候chooseItem還沒有東西，畫面自然不會渲染上去，因此在created或mounted再抓一次localStorage的東西，就能同步顯示，而created或mounted生命週期一前一後而已，兩者差異較細微，基本上寫一樣的東西都會成功
+    this.showTotal(); //在mounted階段呼叫這個methods，得到所有商品加總價錢
+    this.total = localStorage.getItem('total');
+  },
   methods: {
     //======開啟搜尋輸入框======
     showInput() {
@@ -108,32 +96,6 @@ new Vue({
       }
     },
 
-    //======購物車商品數量增加======
-    increase(pointer) {
-      let counts = JSON.parse(localStorage.getItem('items'));
-      for (let i in counts) {
-        if (counts[pointer].item_id == this.chooseItem[i].item_id) {
-          counts[pointer].item_count = parseInt(counts[pointer].item_count) + 1;
-        }
-      }
-      storage.setItem('items', JSON.stringify(counts));
-    },
-
-    //======購物車商品數量減少======
-    decrease(pointer) {
-      let counts = JSON.parse(localStorage.getItem('items'));
-      for (let i in counts) {
-        if (
-          counts[pointer].item_id == this.chooseItem[i].item_id &&
-          counts[pointer].item_count > 1
-          //商品數量大於1才能減，直到最小為1
-        ) {
-          counts[pointer].item_count = parseInt(counts[pointer].item_count) - 1;
-        }
-      }
-      storage.setItem('items', JSON.stringify(counts));
-    },
-
     //======商品加到購物車======
     addToCart(product) {
       alert('成功加入購物車');
@@ -145,6 +107,7 @@ new Vue({
         item_name: product.PRODUCT_NAME,
         item_count: product.count,
         item_price: product.PRODUCT_PRICE,
+        item_total_price: product.PRODUCT_PRICE, //計算總價用，初始值為各商品初始價錢
       };
       // 設定localStorage的value要放的資料
 
@@ -158,6 +121,10 @@ new Vue({
             value[i].item_count = parseInt(value[i].item_count) + 1;
             repeat = true;
             //如果value的item_id等於cart的item_id，數量直接+1，並且repeat此時變更為true
+
+            //單樣商品價錢更新，總價為初始價錢*數量，並賦值給item_total_price
+            value[i].item_total_price =
+              parseInt(value[i].item_price) * parseInt(value[i].item_count);
           }
         }
         if (!repeat) {
@@ -175,6 +142,50 @@ new Vue({
       }
     },
 
+    //======購物車商品數量增加======
+    increase(pointer) {
+      let counts = JSON.parse(localStorage.getItem('items'));
+      for (let i in counts) {
+        if (counts[pointer].item_id == this.chooseItem[i].item_id) {
+          counts[pointer].item_count = parseInt(counts[pointer].item_count) + 1;
+
+          //價錢更新，總價為初始價錢*數量，並賦值給item_total_price
+          counts[pointer].item_total_price =
+            parseInt(counts[pointer].item_price) *
+            parseInt(counts[pointer].item_count);
+        }
+      }
+
+      //因為只針對localStorage改動，而畫面是抓chooseItem的資料渲染的，所以要再讓chooseItem等於改動後的counts陣列，畫面抓chooseItem才是改動後的資料
+      this.chooseItem = counts;
+      this.showTotal();
+      storage.setItem('items', JSON.stringify(counts));
+    },
+
+    //======購物車商品數量減少======
+    decrease(pointer) {
+      let counts = JSON.parse(localStorage.getItem('items'));
+      for (let i in counts) {
+        if (
+          counts[pointer].item_id == this.chooseItem[i].item_id &&
+          counts[pointer].item_count > 1
+          //商品數量大於1才能減，直到最小為1
+        ) {
+          counts[pointer].item_count = parseInt(counts[pointer].item_count) - 1;
+
+          //價錢更新，總價為初始價錢*數量，並賦值給item_total_price
+          counts[pointer].item_total_price =
+            parseInt(counts[pointer].item_price) *
+            parseInt(counts[pointer].item_count);
+        }
+      }
+
+      //因為只針對localStorage改動，而畫面是抓chooseItem的資料渲染的，所以要再讓chooseItem等於改動後的counts陣列，畫面抓chooseItem才是改動後的資料
+      this.chooseItem = counts;
+      this.showTotal();
+      storage.setItem('items', JSON.stringify(counts));
+    },
+
     //======商品從購物車移除======
     removeToCart(pointer) {
       let check_delete = confirm('是否移除?');
@@ -185,6 +196,7 @@ new Vue({
             //相互比對ID
             counts.splice(i, 1); //localStorage刪除
             this.chooseItem.splice(pointer, 1); //畫面刪除
+            this.showTotal();
           }
         }
         storage.setItem('items', JSON.stringify(counts));
@@ -199,6 +211,7 @@ new Vue({
     //======開啟購物車======
     openCart() {
       this.chooseItem = JSON.parse(localStorage.getItem('items'));
+      this.showTotal();
       var shoppingcartbk = document.getElementById('i_shoppingCart_bk');
       if (shoppingcartbk.style.display === 'none') {
         shoppingcartbk.style.display = 'block';
@@ -213,9 +226,21 @@ new Vue({
         });
       }
     },
+
     into(pointer) {
       this.currentPage.push(this.info[pointer]);
       console.log(this.info[this.page]);
+    },
+
+    // ======所有商品總價計算======
+    showTotal() {
+      this.total = 0;
+      for (let i in this.chooseItem) {
+        this.total += parseInt(this.chooseItem[i].item_total_price);
+      }
+      // 對目前chooseItem裡的項目跑回圈，去加總每一項商品總價
+      localStorage.setItem('total', JSON.stringify(this.total));
+      // 計算完轉成字串加到localStorage
     },
   },
   computed: {
@@ -239,4 +264,38 @@ new Vue({
       }
     },
   },
+});
+
+//======卡號輸入自動跳下一格======
+document.addEventListener('DOMContentLoaded', function () {
+  var cards = document.getElementsByClassName('card');
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener('keydown', function (e) {
+      if (
+        (e.which >= 48 && e.which <= 57 && e.which <= 105 && e.which >= 96) ||
+        e.which == 8
+      ) {
+        if (e.target.value.length == 0 && e.which == 8) {
+          let previous_el = this.previousElementSibling;
+          if (previous_el != null) {
+            previous_el.focus();
+          }
+        }
+      } else {
+        e.preventDefault();
+      }
+    });
+
+    // keyup
+    cards[i].addEventListener('keyup', function (e) {
+      let str = e.target.value.replace(/\D/g, '');
+      e.target.value = str;
+      if (str.length == 4) {
+        let next_el = this.nextElementSibling;
+        if (next_el != null) {
+          next_el.focus();
+        }
+      }
+    });
+  }
 });
