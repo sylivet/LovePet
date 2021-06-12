11861ec949a05c197ee5bfddd93b730fdcdd728b
sylivet <yuwenlavie@gmail.com>
2021-06-12 16:19:58 +0800

    健檢預約可不強制選加價購了

--- dev/php/I_conn.php
+++ dev/php/I_conn.php
@@ -3,7 +3,6 @@
 //MySQL相關資訊
 $db_host = "127.0.0.1";
 $db_user = "root";
-$db_pass = "password";
 $db_select = "LOVE_PET";
 $db_pass = "ac1234zxc";
 
--- dev/php/front_end_API/C_Insert_booking.php
+++ dev/php/front_end_API/C_Insert_booking.php
@@ -2,7 +2,6 @@
 
        include("../../php/C_conn.php");
 
-
        //取得POST過來的值
        $FK_MEMBER_ID = $_POST["FK_MEMBER_ID"];
        $FK_PET_ID = $_POST["FK_PET_ID"];
@@ -12,12 +11,10 @@
        $FK_HELTH_CHECK_ID = $_POST["FK_HELTH_CHECK_ID"];
        $FK_HELTH_CHECK_ID2 = $_POST["FK_HELTH_CHECK_ID2"];
        $TOTAL_PRICE = $_POST["TOTAL_PRICE"];
-     
-
-
+      
       //建立SQL
+      if($FK_HELTH_CHECK_ID&&$FK_HELTH_CHECK_ID2){
       $sql = "INSERT INTO HOSPITAL_ORDER(FK_MEMBER_ID,FK_PET_ID, CREATE_DATE, BOOKING_DATE, FK_SET_MENU_ID, FK_HELTH_CHECK_ID, FK_HELTH_CHECK_ID2, TOTAL_PRICE, ORDER_STATUS) VALUES (?, ?, ?, ?, ?, ?, ?, ?, 0)";
-              
       //執行
       $statement = $pdo->prepare($sql);  
 
@@ -29,8 +26,36 @@
       $statement->bindValue(6 , $FK_HELTH_CHECK_ID); 
       $statement->bindValue(7 , $FK_HELTH_CHECK_ID2); 
       $statement->bindValue(8 , $TOTAL_PRICE); 
-
       $statement->execute();
+     }else if($FK_HELTH_CHECK_ID&&!$FK_HELTH_CHECK_ID2){
+      $sql = "INSERT INTO HOSPITAL_ORDER(FK_MEMBER_ID,FK_PET_ID, CREATE_DATE, BOOKING_DATE, FK_SET_MENU_ID, FK_HELTH_CHECK_ID, TOTAL_PRICE, ORDER_STATUS) VALUES (?, ?, ?, ?, ?, ?, ?, 0)";
+      //執行
+      $statement = $pdo->prepare($sql);  
+      $statement->bindValue(1 , $FK_MEMBER_ID); 
+      $statement->bindValue(2 , $FK_PET_ID); 
+      $statement->bindValue(3 , $CREATE_DATE); 
+      $statement->bindValue(4 , $BOOKING_DATE); 
+      $statement->bindValue(5 , $FK_SET_MENU_ID); 
+      $statement->bindValue(6 , $FK_HELTH_CHECK_ID); 
+      $statement->bindValue(7 , $TOTAL_PRICE); 
+      $statement->execute();     
+      }else{
+            $sql = "INSERT INTO HOSPITAL_ORDER(FK_MEMBER_ID,FK_PET_ID, CREATE_DATE, BOOKING_DATE, FK_SET_MENU_ID, TOTAL_PRICE, ORDER_STATUS) VALUES (?, ?, ?, ?, ?, ?, 0)";      
+            //執行
+            $statement = $pdo->prepare($sql);  
+
+            $statement->bindValue(1 , $FK_MEMBER_ID); 
+            $statement->bindValue(2 , $FK_PET_ID); 
+            $statement->bindValue(3 , $CREATE_DATE); 
+            $statement->bindValue(4 , $BOOKING_DATE); 
+            $statement->bindValue(5 , $FK_SET_MENU_ID); 
+            $statement->bindValue(6 , $TOTAL_PRICE); 
+      
+            $statement->execute();
+      }
+      
+              
+
        
 
 
