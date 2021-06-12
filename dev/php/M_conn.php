f7903a493d53066af5bbd126b7bc9f3ca1b05e03
tonyke1016 <ko3chutony831016@gmail.com>
2021-06-12 12:46:27 +0800

    改改

--- dev/php/M_conn.php
+++ dev/php/M_conn.php
@@ -1,19 +1,10 @@
 <?php
 
-<<<<<<< HEAD
-//MySQL相關資訊
-$db_host = "127.0.0.1";
-$db_user = "root";
-$db_select = "LOVE_PET";
-$db_pass = "kevin820304";
-
-=======
   //MySQL相關資訊
   $db_host = "127.0.0.1";
   $db_user = "root";
   $db_pass = "password";
   $db_select = "LOVE_PET";
->>>>>>> Tony
 
 
 //建立資料庫連線物件
