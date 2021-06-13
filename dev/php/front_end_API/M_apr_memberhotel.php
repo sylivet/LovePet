d3911c9fe597a914d21ca80062c82972825b1484
eddieco-cpu <eddie01241@gmail.com>
2021-06-13 19:05:48 +0800

    新增撈訂單php

---
+++ dev/php/front_end_API/M_apr_memberhotel.php
@@ -0,0 +1,26 @@
+<?php
+
+$input_account = $_POST["MID"];
+//$input_account = 1;
+
+// ========= 取得與資料庫連線 ========== //
+include("../M_conn.php");
+
+
+//============================= 引入資料 =====================================//
+
+$sql = "SELECT *,DATE(CREATE_DATE) as CREATE_DATE2 FROM HOTEL_ORDER ho left join ROOM_TYPE rt on
+ho.FK_ROOM_TYPE_ID = rt.ROOM_TYPE_ID WHERE (`FK_MEMBER_ID` = ?);";
+    
+    //執行並查詢，會回傳查詢結果的物件，必須使用fetch、fetchAll...等方式取得資料
+    $statement = $pdo->prepare($sql);    
+    $statement->bindValue(1, $input_account);
+
+    $statement->execute();
+    
+    //抓出全部且依照順序封裝成一個二維陣列
+    $data = $statement->fetchAll(PDO::FETCH_ASSOC); 
+
+    //print_r($data);
+    echo  json_encode($data);  
+?>
